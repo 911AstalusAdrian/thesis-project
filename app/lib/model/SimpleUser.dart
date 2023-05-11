332e21acd84d38332c5446f7b64d74a564409222
Adrian Astalus <astalus.adrian1703@gmail.com>
2023-05-11 20:04:18 +0300

    Login screen and redirect to main page

---
+++ app/lib/model/SimpleUser.dart
@@ -0,0 +1,9 @@
+class SimpleUser {
+  final String email;
+  final String password;
+
+  const SimpleUser({
+    required this.email,
+    required this.password
+  });
+}
---
+++ app/lib/screens/loginScreen.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+
+import '../model/SimpleUser.dart';
+import 'mainScreen.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Icon(
+                  Icons.person_outlined,
+                  size: 150.0,
+                  color: Colors.orange,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 10.0, horizontal: 20.0),
+                  child: TextFormField(
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter an e-mail!';
+                      }
+                      return null;
+                    },
+                    decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'E-mail',
+                        hintText: 'john@gmail.com'),
+                    controller: emailController,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(20, 10, 20, 0),
+                  child: TextFormField(
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter a password!';
+                      }
+                      return null;
+                    },
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Password',
+                    ),
+                    controller: passwordController,
+                  ),
+                ),
+                TextButton(
+                    onPressed: () {
+                      print('Redirect to forgot password');
+                    },
+                    child: const Text('Forgot my password')),
+                ElevatedButton(
+                  onPressed: () async {
+                    if (_formKey.currentState!.validate()) {
+                      final email = emailController.value.text;
+                      final password = passwordController.value.text;
+                      //TODO here we should get the username which has the matching email and password and to pass on
+                      // final SimpleUser user = SimpleUser(email: email, password: password);
+                      await Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => MainScreen()));
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 10.0, horizontal: 30.0),
+                      backgroundColor: Colors.orangeAccent,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30))),
+                  child: const Text('Log In',
+                      style: TextStyle(fontSize: 22.0, color: Colors.white)),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    const Text('Don\'t have an account?'),
+                    TextButton(onPressed: () {}, child: const Text('Sign In'))
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
