973c09e6e6d8ce9676945dfbdca0e907b47080bd
Adrian Astalus <astalus.adrian1703@gmail.com>
2023-05-24 13:07:08 +0300

    Work on Profile Card

--- app/lib/screens/profileScreen.dart
+++ app/lib/screens/profileScreen.dart
@@ -1,60 +1,56 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-
-import '../model/user_model.dart';
 import '../server/server.dart';
+import '../widgets/profileCard.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({Key? key}) : super(key: key);
-
 
   @override
   State<ProfileScreen> createState() => _ProfileScreenState();
 }
 
-
-
 class _ProfileScreenState extends State<ProfileScreen> {
-
   Server server = Server();
   final String uid = FirebaseAuth.instance.currentUser!.uid;
   CollectionReference users = FirebaseFirestore.instance.collection("Users");
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
   }
 
-
   @override
   Widget build(BuildContext context) {
-    // TODO: Investigate why do I get a Stack Overflow if I try to create the card as a Stateless Widget
-    // TODO  in Scaffold, tried the following: body : ProfileCard(user: widget.user)
-    // TODO can do without a widget but it would be a nice thing
-
-
     return Center(
-      child: FutureBuilder<DocumentSnapshot>(
-          future: users.doc(uid).get(), // the only way this thing works
-          builder: (BuildContext ctx, AsyncSnapshot<DocumentSnapshot> snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(child: CircularProgressIndicator()); }
-            else if (snapshot.hasError) {
-              return Center(child: Text( '${snapshot.error} occurred'));}
-            else if (snapshot.hasData) {
-              final String data = snapshot.data!.get("eMail");
-              return Center(
-                child: Text(
-                  data,
-                  style: TextStyle(fontSize: 18),
-                ),
-              );
-            }
-            else { return const Center(child: CircularProgressIndicator()); }
-          })
-    );
-  }
+        child: FutureBuilder<DocumentSnapshot>(
+            future: users.doc(uid).get(), // the only way this thing works
+            builder:
+                (BuildContext ctx, AsyncSnapshot<DocumentSnapshot> snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator());
+              } else if (snapshot.hasError) {
+                return Center(child: Text('${snapshot.error} occurred'));
+              } else if (snapshot.hasData) {
+                final data = snapshot.data!;
 
+                return SafeArea(
+                  child: Column(
+                    children: [
+                      Align(
+                        alignment: Alignment.topCenter,
+                        child: ProfileCard(data: data)),
+                      TextButton(
+                          onPressed: () {},
+                          child: const Text("Change password"))
+                    ],
+                  ));
+
+                // return ProfileCard(data: data);
+              } else {
+                return const Center(child: CircularProgressIndicator());
+              }
+            }));
+  }
 }
--- app/lib/widgets/profileCard.dart
+++ app/lib/widgets/profileCard.dart
@@ -1,14 +1,83 @@
+import 'package:app/assets/Colors.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 import '../model/user_model.dart';
 
-class ProfileCard extends StatelessWidget {
-  const ProfileCard({Key? key, required this.user}) : super(key: key);
+class ProfileCard extends StatefulWidget {
+  const ProfileCard({Key? key, required this.data}) : super(key: key);
 
-  final UserModel user;
+  final DocumentSnapshot<Object?> data;
+
+  @override
+  State<ProfileCard> createState() => _ProfileCardState();
+}
+
+class _ProfileCardState extends State<ProfileCard> {
+
+  TextEditingController fNameCtrl = TextEditingController();
+  bool fNameTextFieldEnabled = false;
 
   @override
   Widget build(BuildContext context) {
-    return Text(user.fName);
+
+    final UserModel user = UserModel(fName: widget.data.get('fName'),
+        lName: widget.data.get('fName'),
+        userName: widget.data.get('fName'),
+        eMail: widget.data.get('fName'));
+
+    fNameCtrl.text = user.fName;
+
+
+    return Container(
+        width: double.infinity,
+        margin: const EdgeInsets.fromLTRB(20.0, 60.0, 20.0, 10.0),
+        child: Card(
+            color: myGrey.withOpacity(0.5),
+            elevation: 20,
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              child: Column(
+                children: [
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: Icon(Icons.person_outline, size: 50.0, color: myOrange),
+                  ),
+                  Row(
+                    children: [
+                      const Text("First Name:"),
+                      Row(children: [
+                        Container(
+                          width: 50,
+                          child: TextField(
+                              enabled: fNameTextFieldEnabled,
+                              decoration: const InputDecoration(
+                                  border: InputBorder.none,
+                                  contentPadding: EdgeInsets.all(0)),
+                              controller: fNameCtrl
+                          ),
+                        ),
+                        IconButton(
+
+                          onPressed: () {
+                            setState(() {
+                              fNameTextFieldEnabled = !fNameTextFieldEnabled;
+                            });
+                          },
+                          icon: Icon(Icons.edit),
+                        )
+                      ],),
+                      const SizedBox(width: 30.0),
+                      const Text("Last Name:"),
+                      Text(user.lName),
+                    ],
+                  ),
+
+                ],
+              ),
+            )
+        )
+    );
   }
 }
