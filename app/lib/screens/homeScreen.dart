d8bfc99da6dbc9c39d70ed6ec9b35c0185909fe2
Adrian Astalus <astalus.adrian1703@gmail.com>
2023-06-10 20:07:13 +0300

    Handle login password reset

--- app/lib/screens/homeScreen.dart
+++ app/lib/screens/homeScreen.dart
@@ -25,69 +25,71 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          FutureBuilder(
-            future: server.getName(),
-            builder: (BuildContext ctx, AsyncSnapshot<dynamic> snapshot) {
-              if (snapshot.hasData) {
-                String name = snapshot.data!;
-                return FancyText(text: "Hello, $name!");
-              } else {
-                return const Center(child: Text("Something went wrong"));
-              }
-            },
-          ),
-          const FancyText(text: "Here's how your upcoming Trips look like:"),
-          Container(
-            child: FutureBuilder(
-                future: server.getOwnedTrips(),
-                builder: (BuildContext ctx, AsyncSnapshot<dynamic> snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const Center(child: CircularProgressIndicator());
-                  } else if (snapshot.hasError) {
-                    return Center(child: Text('${snapshot.error} has occurred'));
-                  } else if (snapshot.hasData) {
-                    List<Map<String, dynamic>> data = snapshot.data!;
-                    if (data.isEmpty) {
-                      return const Center(
-                          child: Text("You have to Trips planned yet!"));
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            FutureBuilder(
+              future: server.getName(),
+              builder: (BuildContext ctx, AsyncSnapshot<dynamic> snapshot) {
+                if (snapshot.hasData) {
+                  String name = snapshot.data!;
+                  return FancyText(text: "Hello, $name!");
+                } else {
+                  return const Center(child: Text("Please wait..."));
+                }
+              },
+            ),
+            const FancyText(text: "Here's how your upcoming Trips look like:"),
+            Container(
+              child: FutureBuilder(
+                  future: server.getOwnedTrips(),
+                  builder: (BuildContext ctx, AsyncSnapshot<dynamic> snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const Center(child: CircularProgressIndicator());
+                    } else if (snapshot.hasError) {
+                      return Center(child: Text('${snapshot.error} has occurred'));
+                    } else if (snapshot.hasData) {
+                      List<Map<String, dynamic>> data = snapshot.data!;
+                      if (data.isEmpty) {
+                        return const Center(
+                            child: Text("You have to Trips planned yet!"));
+                      } else {
+                        return SizedBox(
+                          height: 600,
+                          child: ListView.builder(
+                              itemCount: data.length,
+                              itemBuilder: (BuildContext ctx, int index) {
+
+                                DateTime currentDate = DateTime.now();
+                                currentDate = DateTime(currentDate.year, currentDate.month, currentDate.day);
+                                DateTime startDate = data[index]['startDate'].toDate();
+                                Duration daysLeft = startDate.difference(currentDate);
+                                int noOfDays = daysLeft.inDays;
+
+                                String tripID = data[index]['tripID'];
+                                BasicTripModel trip = BasicTripModel.fromJson(data[index]);
+
+                                return ListTile(
+                                  leading: const Icon(Icons.flight_takeoff),
+                                  title: Text(data[index]['title']),
+                                  subtitle: noOfDays == 0 ? const Text("Today") : Text("$noOfDays day(s) left"),
+                                  trailing: TextButton(
+                                    onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (context) => ItineraryScreen(tripID: tripID, trip: trip,))),
+                                    child: const Text("View Itinerary"),
+                                  ),
+                                );
+                              }),
+                        );
+                      }
                     } else {
-                      return SizedBox(
-                        height: 600,
-                        child: ListView.builder(
-                            itemCount: data.length,
-                            itemBuilder: (BuildContext ctx, int index) {
-
-                              DateTime currentDate = DateTime.now();
-                              currentDate = DateTime(currentDate.year, currentDate.month, currentDate.day);
-                              DateTime startDate = data[index]['startDate'].toDate();
-                              Duration daysLeft = startDate.difference(currentDate);
-                              int noOfDays = daysLeft.inDays;
-
-                              String tripID = data[index]['tripID'];
-                              BasicTripModel trip = BasicTripModel.fromJson(data[index]);
-
-                              return ListTile(
-                                leading: const Icon(Icons.flight_takeoff),
-                                title: Text(data[index]['title']),
-                                subtitle: noOfDays == 0 ? const Text("Today") : Text("$noOfDays day(s) left"),
-                                trailing: TextButton(
-                                  onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (context) => ItineraryScreen(tripID: tripID, trip: trip,))),
-                                  child: const Text("View Itinerary"),
-                                ),
-                              );
-                            }),
-                      );
+                      return const Center(child: Text("Yeah what happened here"));
                     }
-                  } else {
-                    return const Center(child: Text("Yeah what happened here"));
-                  }
-                }),
-          )
-        ],
-      ),
+                  }),
+            )
+          ],
+        ),
+      )
     );
   }
 }
--- app/lib/screens/loginScreen.dart
+++ app/lib/screens/loginScreen.dart
@@ -1,6 +1,8 @@
 import 'package:app/screens/mainNavigationScreen.dart';
 import 'package:app/screens/signInScreen.dart';
+import 'package:app/widgets/fancyText.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
@@ -12,7 +14,6 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-
   final _formKey = GlobalKey<FormState>();
   final _secureStorage = const FlutterSecureStorage();
   final FirebaseAuth _auth = FirebaseAuth.instance;
@@ -52,9 +53,10 @@ class _LoginScreenState extends State<LoginScreen> {
                       return null;
                     },
                     decoration: const InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'E-mail',
-                        hintText: 'john@gmail.com'),
+                      border: OutlineInputBorder(),
+                      labelText: 'E-mail',
+                      hintText: 'john@gmail.com',
+                    ),
                     controller: emailController,
                   ),
                 ),
@@ -79,11 +81,15 @@ class _LoginScreenState extends State<LoginScreen> {
                 ),
                 TextButton(
                     onPressed: () {
-                      print('Redirect to forgot password');
+                      _forgotPasswordDialog();
                     },
                     child: const Text('Forgot my password')),
                 ElevatedButton(
-                  onPressed: () async { if (_formKey.currentState!.validate()) { _login(); }},
+                  onPressed: () async {
+                    if (_formKey.currentState!.validate()) {
+                      _login();
+                    }
+                  },
                   style: ElevatedButton.styleFrom(
                       padding: const EdgeInsets.symmetric(
                           vertical: 10.0, horizontal: 30.0),
@@ -97,9 +103,14 @@ class _LoginScreenState extends State<LoginScreen> {
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
                     const Text('Don\'t have an account?'),
-                    TextButton(onPressed: () {
-                      Navigator.push(context, MaterialPageRoute(builder: (context) => const SignInScreen()));
-                    }, child: const Text('Sign In'))
+                    TextButton(
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const SignInScreen()));
+                        },
+                        child: const Text('Sign In'))
                   ],
                 )
               ],
@@ -110,25 +121,101 @@ class _LoginScreenState extends State<LoginScreen> {
     );
   }
 
-   _login() async {
+  _login() async {
+    final email = emailController.text.trim();
+    final password = passwordController.text.trim();
 
-     final email = emailController.text.trim();
-     final password = passwordController.text.trim();
+    try {
+      final credential = await _auth.signInWithEmailAndPassword(
+          email: email, password: password);
 
-     try {
-
-       final credential = await _auth.signInWithEmailAndPassword(
-           email: email,
-           password: password);
-
-       _secureStorage.write(key: "uid", value: credential.user!.uid);
-       if (!mounted) return;
-       await Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => MainNavigationScreen(index: 0)));
-
-     } on FirebaseAuthException catch (e) {
-       if (e.code == 'user-not-found') { print('No user found for that email.'); }
-       else if (e.code == 'wrong-password') { print('Wrong password provided for that user.'); }
-     }
+      _secureStorage.write(key: "uid", value: credential.user!.uid);
+      if (!mounted) return;
+      await Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+              builder: (context) => MainNavigationScreen(index: 0)));
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        print("user-not-found error");
+        showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: const Text("User not found!"),
+                content: const Text("Please try again"),
+                actions: [
+                  TextButton(
+                      onPressed: () => Navigator.of(context).pop(),
+                      child: const Text("OK"))
+                ],
+              );
+            });
+      } else if (e.code == 'wrong-password') {
+        print('Wrong password provided for that user.');
+        showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: const Text("Invalid password!"),
+                content: const Text("Please try again"),
+                actions: [
+                  TextButton(
+                      onPressed: () => Navigator.of(context).pop(),
+                      child: const Text("OK"))
+                ],
+              );
+            });
+      } else {
+        print(e.code);
+      }
+    }
   }
 
+  _resetPassword(String email) async {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    try{
+      await auth.sendPasswordResetEmail(email: email);
+    } catch (e) {
+      print("PASSWORD RESET ERROR");
+    }
+  }
+
+  _forgotPasswordDialog() async {
+    TextEditingController email = TextEditingController();
+
+    Dialog passwordReset = Dialog(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10)),
+        child: SizedBox(
+          height: 175,
+          child: Column(
+            children: [
+              const FancyText(text: "Forgot my password"),
+              SizedBox(
+                  width: 200,
+                  child: TextField(
+                      controller: email,
+                    decoration: const InputDecoration(hintText: "Enter your e-mail"),
+                  )),
+              ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 10.0, horizontal: 30.0),
+                      backgroundColor: Colors.orangeAccent,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30))),
+                  onPressed: () {
+                    _resetPassword(email.text);
+                    Navigator.of(context).pop();},
+                    child: const Text("Send e-mail")),
+              TextButton(
+                  onPressed: () => Navigator.of(context).pop(),
+                  child: const Text("Cancel"))
+            ],
+          ),
+        ));
+
+    showDialog(context: context, builder: (BuildContext ctx) => passwordReset);
+  }
 }
